c6b7c97407f5dd7f815815e7694e3caf9ae08124
romankhiropulos <romankhiropulos@gmail.com>
2022-10-04 17:17:53 +0300

    first commit

---
+++ src/main/java/ru/job4j/job4j_fast_foot/Job4jFastFootApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.job4j_fast_foot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jFastFootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Job4jFastFootApplication.class, args);
+	}
+
+}
