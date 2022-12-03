44e12d7027596d493bf142a15baa69971409c3d5
romankhiropulos <romankhiropulos@gmail.com>
2022-12-03 21:16:16 +0300

    complete 1. Синхронный обмен сообщений [#458496]

--- admin/src/main/java/ru/job4j/admin/controller/AdminController.java
+++ admin/src/main/java/ru/job4j/admin/controller/AdminController.java
@@ -1,12 +1,12 @@
 package ru.job4j.admin.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import ru.job4j.admin.service.DishService;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class AdminController {
 
--- dish/src/main/java/ru/job4j/dish/repository/DishRepositoryMemory.java
+++ dish/src/main/java/ru/job4j/dish/repository/DishRepositoryMemory.java
@@ -18,19 +18,19 @@ public class DishRepositoryMemory {
      {
          dishes = new ConcurrentHashMap<>();
 
-         Category meat = Category.builder().name("Meat").build();
-         Category vegetable = Category.builder().name("Vegetables").build();
-         Category fruit = Category.builder().name("Fruits").build();
+         Category meat = Category.builder().name("Мясо").build();
+         Category vegetable = Category.builder().name("Овощи").build();
+         Category fruit = Category.builder().name("Фрукты").build();
 
          Type steak = Type.builder().category(meat).name("Стейк").build();
          Type apple = Type.builder().category(fruit).name("Яблоко").build();
          Type tomato = Type.builder().category(vegetable).name("Помидор").build();
          Type potato = Type.builder().category(vegetable).name("Картофель").build();
 
-         dishes.put(1L, Dish.builder().id(1L).category(meat).type(steak).name("Стейк говядины").build());
-         dishes.put(2L, Dish.builder().id(2L).category(fruit).type(apple).name("Апорт Алма-Атинский").build());
-         dishes.put(3L, Dish.builder().id(3L).category(vegetable).type(tomato).name("Томат \"Бычье сердце\"").build());
-         dishes.put(4L, Dish.builder().id(4L).category(vegetable).type(potato).name("Картофель мытый").build());
+         dishes.put(1L, Dish.builder().id(1L).category(meat).type(steak).name("Стейк говядины").cost(199f).cost(199f).build());
+         dishes.put(2L, Dish.builder().id(2L).category(fruit).type(apple).name("Апорт Алма-Атинский").cost(349f).build());
+         dishes.put(3L, Dish.builder().id(3L).category(vegetable).type(tomato).name("Томат \"Бычье сердце\"").cost(5969f).build());
+         dishes.put(4L, Dish.builder().id(4L).category(vegetable).type(potato).name("Картофель мытый").cost(466f).build());
      }
 
      public Collection<Dish> findAll() {
